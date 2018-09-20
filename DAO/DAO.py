ac836039ecdeff2ad1321867ec5e5f6e1a5ba2f9
Henrique Fenandes <henrique@Asgard>
2018-09-20 17:06:36 -0300

    melhorias no método para persistir os dados.

--- DAO/DAO.py
+++ DAO/DAO.py
@@ -1,9 +1,20 @@
-import connect
-
-conn = connect.cria_conexao()
-cursor = conn.cursor()
+import sqlite3
 
 
-def inserir_dados(componente, versao, responsavel, status, data):
-    cursor.execute(f"INSERT INTO deploy VALUES ({componente}, {versao}, {responsavel}, "
-                   f"{status}, {data})")
+def inserir_dados(dados):
+    print(dados.c)
+    global cursor
+    try:
+        conn = sqlite3.connect('deploy.db')
+        cursor = conn.cursor()
+    except Exception as e:
+        print("Erro ao conectar ao bd.", str(e))
+    try:
+        cursor.execute(f"""INSERT INTO deploy (componente, versao, responsavel, status, data) VALUES ('{dados.c}',{float(dados.v)}, '{dados.r}', '{dados.s}', '{dados.d}')""")
+        conn.commit()
+        cursor.close()
+        return True
+    except Exception as e:
+        print("Não foi possível salvar os dados", str(e))
+        cursor.close()
+        return False
--- connect.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-
-def cria_conexao():
-    conn = sqlite3.connect('deploy.db')
-
-    return conn
