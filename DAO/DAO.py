9d9bc6a7ca36a1eaf6acccd9c09dcf102582c3f0
Henrique Fenandes <henrique@Asgard>
2018-09-23 15:35:28 -0300

    Implementação de arquivo de log.

--- DAO/DAO.py
+++ DAO/DAO.py
@@ -1,4 +1,7 @@
-import sqlite3, datetime
+import sqlite3, datetime, logging
+
+logging.basicConfig(filename='deploy_api.log', level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s %(funcName)s => %(message)s')
 
 
 def inserir_dados(dados):
@@ -8,13 +11,15 @@ def inserir_dados(dados):
         conn = sqlite3.connect('deploy.db')
         cursor = conn.cursor()
     except Exception as e:
-        print("Erro ao conectar ao bd.", str(e))
+        logging.info("Erro ao conectar ao bd.")
+        logging.exception(str(e))
     try:
         cursor.execute(f"""INSERT INTO deploy (componente, versao, responsavel, status, data) VALUES ('{dados.c}',{float(dados.v)}, '{dados.r}', '{dados.s}', '{datetime.datetime.now()}')""")
         conn.commit()
         cursor.close()
         return True
     except Exception as e:
-        print("Não foi possível salvar os dados", str(e))
+        logging.info("Não foi possível salvar os dados")
+        logging.exception(str(e))
         cursor.close()
         return False
--- app.py
+++ app.py
@@ -5,9 +5,13 @@
 from flask import Flask, jsonify
 from flask_restful import reqparse
 from DAO import DAO
+import logging
+
+logging.basicConfig(filename='deploy_api.log', level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s %(funcName)s => %(message)s')
 
 
-app = Flask(__name__)
+app = Flask("deploy_api")
 
 parser = reqparse.RequestParser()
 parser.add_argument('c', type=str, required=True)
@@ -22,10 +26,14 @@ def deploy():
 
     ret = DAO.inserir_dados(args)
     if ret:
+        logging.info("Dados inseridos com sucesso.")
+        logging.debug(args)
         return jsonify({"status": "Sucesso", "message": "Dados inseridos com sucesso!"})
     else:
+        logging.info("Erro ao inserir os dados.")
+        logging.debug(args)
         return jsonify({"status": "Erro", "code": 500, "data": None, "message": "Ocorreu um erro ao inserir as informações."})
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)
